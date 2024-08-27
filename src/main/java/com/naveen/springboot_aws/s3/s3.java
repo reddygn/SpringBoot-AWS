23725f36fa3c173452d8f272c5283806269683e4
Naveen Reddy <69370237+reddygn@users.noreply.github.com>
2024-08-27 11:31:34 -0500

    init commit

---
+++ src/main/java/com/naveen/springboot_aws/s3/s3.java
@@ -0,0 +1,8 @@
+package com.naveen.springboot_aws.s3;
+
+/**
+ * @author Naveen Reddy
+ * @project springboot-aws
+ */
+public class s3 {
+}
---
+++ src/test/java/com/naveen/springboot_aws/SpringbootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naveen.springboot_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
